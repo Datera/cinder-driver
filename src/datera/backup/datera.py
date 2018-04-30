0450d47f47aa973cfdcd0d26b378344ac7fff5c5
Matt Smith <mss@datera.io>
2018-04-30 10:41:03 -0500

    Switching versioning in backup driver to date-based

--- src/datera/backup/datera.py
+++ src/datera/backup/datera.py
@@ -141,9 +141,10 @@ class DateraBackupDriver(chunkeddriver.ChunkedBackupDriver):
     Version history:
         1.0.0 - Initial driver
         1.0.1 - Added secondary backup driver dispatching/multiplexing
+        2018.5.1.0 - Switched to date-based versioning scheme
     """
 
-    VERSION = '1.0.1'
+    VERSION = '2018.5.1.0'
 
     HEADER_DATA = {'Datera-Driver': 'OpenStack-Backup-{}'.format(VERSION)}
 
